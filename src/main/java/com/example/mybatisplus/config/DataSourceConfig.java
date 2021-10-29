cf793b9b6a6f1987bd937b4a488a62ee34f875e7
朱运升 <yunsheng.zhu@71360.com>
2021-10-29 15:54:38 +0800

    反射

---
+++ src/main/java/com/example/mybatisplus/config/DataSourceConfig.java
@@ -0,0 +1,9 @@
+package com.example.mybatisplus.config;
+
+/**
+ *  *@Description          
+ *  *@Author  zhuyunsheng       //作者
+ *  *@Date 2021/7/20 16:40      //创建时间
+ **/
+public class DataSourceConfig {
+}
---
+++ src/test/java/com/example/mybatisplus/servcie/impl/AccountServiceImplTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *  *@Description          
+ *  *@Author  zhuyunsheng       //作者
+ *  *@Date 2021/7/20 17:01      //创建时间
+ **/
+class AccountServiceImplTest {
+
+}
